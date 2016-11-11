5bad92be027327950f69b3e6ee58f85555cd0a53
Tam <tamborinehands@gmail.com>
2016-11-11 17:59:12 +0000

    skeleton of test outputs

---
+++ spec/converter_spec.rb
@@ -0,0 +1,26 @@
+require 'converter'
+
+Rspec.describe Converter do
+
+  it "pish tegj glob glob is 52" do
+
+  end
+
+  it "glob prok silver is 68 credits" do
+
+  end
+
+  it "glob prok gold is 57800 credits" do
+
+  end
+
+  it "glob prok iron is 782 credits" do
+
+  end
+
+  it "raises error for invalid input" do
+
+  end
+
+
+end
