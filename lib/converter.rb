647b37bb3a8e4fe657cb31e241c340ae83432f9d
Tam <tamborinehands@gmail.com>
2016-11-11 19:53:53 +0000

    tests only fail because new roman numeral conversions need to be included

--- lib/converter.rb
+++ lib/converter.rb
@@ -2,14 +2,17 @@ class Converter
 
 #default givens
 NUMERAL_GALAX = { glob: :I, prok: :V, pish: :X, tegj: :L}
-NUMERAL_CREDIT = { I: 1, V: 5, X: 10, L: 50}
-ORE_CREDIT = { silver: 17, gold: 14450, iron: 195}
+NUMERAL_CREDIT = { I: 1.0, V: 5.0, X: 10.0, L: 50.0}
+ORE_CREDIT = { silver: 17.0, gold: 14450.0, iron: 195.0}
 #show the algebra that gave these values
 
   def convert(input_currency)
     input_currency = toArrayOfStrings(input_currency)
     values, multiplier = correctLookUp(input_currency)
-    return values.inject(:+) * multiplier
+    p values
+    p "in here!"
+    p multiplier
+    return values.inject(:+) * (multiplier != 0 ? multiplier : 1)
     # check validity of input or raiseerror
 
   end
@@ -19,11 +22,11 @@ ORE_CREDIT = { silver: 17, gold: 14450, iron: 195}
   def correctLookUp(input_currency)
     values = []
     multiplier = 0
-    input_currency.each do |word|
+    input_currency.each do |word| #[glob, prok, silver]
       word = word.to_sym
       if NUMERAL_GALAX.keys.include?(word)
         p "FIRST CALLED"
-        p NUMERAL_CREDIT[NUMERAL_GALAX[word]]
+        # p NUMERAL_CREDIT[NUMERAL_GALAX[word]]
         values << NUMERAL_CREDIT[NUMERAL_GALAX[word]]
       elsif
         p "SECOND CALLED"
