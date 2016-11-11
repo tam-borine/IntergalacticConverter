32a423492bbfc6ff6657a4aa6c8b81d9255a4bd7
Tam <tamborinehands@gmail.com>
2016-11-11 18:29:01 +0000

    got mvp failing tests

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -3,22 +3,23 @@ require 'converter'
 RSpec.describe Converter do
 
   it "pish tegj glob glob is 52" do
-    expect(subject.convert("pish tegj glob glob")).to eq(52)
+    expect(subject.convert("pish tegj glob glob")).to eq("52")
   end
 
   it "glob prok silver is 68 credits" do
-
+    expect(subject.convert("glob prok silver")).to eq("68 credits")
   end
 
   it "glob prok gold is 57800 credits" do
-
+    expect(subject.convert("glob prok gold")).to eq("57800 credits")
   end
 
   it "glob prok iron is 782 credits" do
-
+    expect(subject.convert("glob prok iron")).to eq("782 credits")
   end
 
   it "raises error for invalid input" do
+    expect{subject.convert(nil)}.to raise "I have no idea what you are talking about"
 
   end
 
