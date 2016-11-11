bf999be7c8f6c8d0edb14a2d0faf40b96f41178f
Tam <tamborinehands@gmail.com>
2016-11-11 20:57:05 +0000

    saved notes

---
+++ notes.rb
@@ -0,0 +1,20 @@
+# Test input:
+# glob is I
+# prok is V
+# pish is X
+# tegj is L
+# glob glob Silver is 34 Credits
+# glob prok Gold is 57800 Credits
+# pish pish Iron is 3910 Credits
+# how much is pish tegj glob glob ? XLII = 42
+# how many Credits is glob prok Silver ?
+# how many Credits is glob prok Gold ? 57800
+# how many Credits is glob prok Iron ?
+# how much wood could a woodchuck chuck if a woodchuck could chuck wood ?
+#
+# Test Output:
+# pish tegj glob glob is 42
+# glob prok Silver is 68 Credits
+# glob prok Gold is 57800 Credits
+# glob prok Iron is 782 Credits
+# I have no idea what you are talking about
