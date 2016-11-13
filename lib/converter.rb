b852653b10bb53207a012c9c39ace48dff072c97
Tam <tamborinehands@gmail.com>
2016-11-13 16:36:47 +0000

    failure undefined method updateGivens YAY FOR TDD

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,7 +1,7 @@
 class Converter
 
 #default givens
-NUMERAL_GALAX = { glob: "I", prok: "V", pish: "X", tegj: "L"}
+NUMERAL_GALAX = { glob: nil, prok: "V", pish: "X", tegj: "L"}
 NUMERAL_CREDIT = { I: 1.0, V: 5.0, X: 10.0, L: 50.0, C: 100.0, D: 500.0, M: 1000.0}
 ORE_CREDIT = { silver: 17.0, gold: 14450.0, iron: 195.5}
 
