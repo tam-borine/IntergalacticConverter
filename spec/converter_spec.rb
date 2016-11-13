f9b9d951abc969e0061ffab8f30b840cf34e9799
Tam <tamborinehands@gmail.com>
2016-11-13 13:21:10 +0000

    skeleton of tests for edge cases on roman numerals

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -3,26 +3,51 @@ require 'converter'
 RSpec.describe Converter do
 subject {described_class.new}
 
-  it "pish tegj glob glob is 42" do
-    expect(subject.convert("pish tegj glob glob")).to eq(42.0)
+  describe "feature tests" do
+
+    it "pish tegj glob glob is 42" do
+      expect(subject.convert("pish tegj glob glob")).to eq(42.0)
+    end
+
+    it "glob prok silver is 68 credits" do
+      expect(subject.convert("glob prok silver")).to eq(68)
+    end
+
+    it "glob prok gold is 57800 credits" do
+      expect(subject.convert("glob prok gold")).to eq(57800)
+    end
+
+    it "glob prok iron is 782 credits" do
+      expect(subject.convert("glob prok iron")).to eq(782)
+    end
+
+    it "raises error for invalid input" do
+      expect{subject.convert(nil)}.to raise_error "I have no idea what you are talking about"
+    end
   end
 
-  it "glob prok silver is 68 credits" do
-    expect(subject.convert("glob prok silver")).to eq(68)
+  describe "#roman_to_integer" do
+    it "old roman numerals are simply translated and summed"
+
+    it "new roman numerals are calculated correctly"
+
+    it "D, L, and V can never repeat"
+
+    it "I, X, C, and M can repeat three times max in succession"
+
+    it "I, X, C, and M can repeat four times if succession is broken by another symbol"
+
+    it "I can be subtracted from V and X only"
+
+    it "X can be subtracted from L and C only"
+
+    it "C can be subtracted from D and M only"
+
+    it "V, L and D can never be subtracted"
+
+
   end
 
-  it "glob prok gold is 57800 credits" do
-    expect(subject.convert("glob prok gold")).to eq(57800)
-  end
-
-  it "glob prok iron is 782 credits" do
-    expect(subject.convert("glob prok iron")).to eq(782)
-  end
-
-  it "raises error for invalid input" do
-    expect{subject.convert(nil)}.to raise_error "I have no idea what you are talking about"
-
-  end
 
 
 end
